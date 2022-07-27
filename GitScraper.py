e861a305b036b38b171918c6af9b1454fa9fc793
Alister Wong <alisterwong@Alisters-Air.modem>
2022-07-27 22:45:46 +1000

    Added flask functionality to accept POST requests
    Is now able to send the alert to discord upon receiving a POST request when the app is run with a flask server

--- GitScraper.py
+++ GitScraper.py
@@ -1,6 +1,8 @@
 from discord_webhooks import DiscordWebhooks
 import urllib.request
 import requests
+from flask import Flask, request, Response
+import json
 
 fwebhook = open("webhook.txt", "r")
 
@@ -17,8 +19,8 @@ class message:
         # Initialize the webhook class and attaches data.
         webhook = DiscordWebhooks(WEBHOOK_URL)
         
-        url = "https://api.github.com/repos/COMP30022-2022/COMP30022/contents/"
-        #url = url + self.folder + "/"
+        url = "https://api.github.com/repos/AlisterWong/COMP30023_Project_2/contents/"
+        url = url + self.folder + "/"
         url = url + self.file
 
         ftoken = open("token.txt", "r")
@@ -40,5 +42,18 @@ class message:
             # Triggers the payload to be sent to Discord.
             webhook.send()
 
-test_message = message("www", "README.md")
-test_message.send_message()
+test_message = message("www", "index.html")
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def return_response():
+    print(request.json)
+    #json_file = json.loads(request.json)
+    #print("\n\n\n" + json_file)
+    #print(json_file["commits"]["id"])
+    #test_message.send_message()
+    return Response(status=200)
+
+
+if __name__ == "__main__": app.run()
