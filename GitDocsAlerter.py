316163b485a53594155f3ceb6bbe2498dd952389
AlisterWong <alister64@live.com.au>
2022-07-28 20:41:41 +1000

    Added documentation to GitDcotsAlerter.py

--- GitDocsAlerter.py
+++ GitDocsAlerter.py
@@ -1,8 +1,21 @@
-from discord_webhooks import DiscordWebhooks
-import urllib.request
+#!/usr/bin/env python
+"""Sends alerts to a Discord webhook based on GitHub text files
+
+This program listens for alerts from GitHub webhooks to run (POST requests).
+It then checks if there were any commits and collects the names of unique
+'.md' files and attempts to send a Discord message with the contents of 
+such files.
+"""
+
 import requests
+from discord_webhooks import DiscordWebhooks
 from flask import Flask, request, Response
-import json
+
+__author__ = "Alister Wong"
+
+__version__ = "1.0"
+__maintainer__ = "Alister Wong"
+__email__ = "alister64@live.com.au"
 
 fwebhook = open("webhook.txt", "r")
 
@@ -15,9 +28,11 @@ class message:
         self.file = file
         
     def send_message(self):
+        """ Sends an alert to discord with the contents of the file """
         # Initialize the webhook class and attaches data.
         webhook = DiscordWebhooks(WEBHOOK_URL)
         
+        # Change this URL to change which repository to pull commits from.
         url = "https://api.github.com/repos/COMP30022-2022/COMP30022/contents/"
         url = url + self.file
 
@@ -26,6 +41,7 @@ class message:
         header = "application/vnd.github.raw"
         token = ftoken.read().strip()
 
+        # Verifies the github token
         resp = requests.get(url, headers={"Accept": header, "Authorization": "token " + token})
         
         if resp.ok:
@@ -35,7 +51,7 @@ class message:
             # Triggers the payload to be sent to Discord.
             webhook.send()
         else:
-            raise Exception("Github API failed: " + self.file)
+            raise Exception("Github token invalid: " + self.file)
 
 
 app = Flask(__name__)
@@ -43,17 +59,22 @@ app = Flask(__name__)
 # Setting route to respond to POST requests
 @app.route('/webhook', methods=['POST'])
 def return_response():
+    """
+    Finds unique filenames and creates a message object to send alerts via
+    the send_message() function.
+    """
     data = request.json
-    
+    # Can modify which file extensions to detect.
+    file_extension = 'md'
     commit_set = set()
 
     # Goes through the commits and adds unique filenames to a set
     for commit in data["commits"]:
         for file in commit["added"]:
-            if file.endswith('.md'):
+            if file.endswith(file_extension):
                 commit_set.add(file)
         for file in commit["modified"]:
-            if file.endswith('.md'):
+            if file.endswith(file_extension):
                 commit_set.add(file)
 
     # Sends discord alert for each unique file in latest commit
